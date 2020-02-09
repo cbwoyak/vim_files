c4ee1d44f6832b6b691419969c679baa6b8dac82
Christopher Woyak <cbwoyak@gmail.com>
2020-02-09 15:15:05 +0100

    Updates to install script

--- install.sh
+++ install.sh
@@ -2,17 +2,37 @@
 
 vim_dir=~/.vim
 vim_bak=~/.vim.bak
+vimrc=~/.vimrc
+vimrc_bak=~/.vimrc.bak
 
-cp vimrc ~/.vimrc
-if [ -d $vim_bak ] && [ -w $vim_bak ]
+# if backup of vimrc exists, delete it
+if [[ -f $vimrc_bak ]]
+then
+  rm $vimrc_bak
+fi
+
+# backup current vimrc
+if [[ -f $vimrc ]]
+then
+  mv $vimrc $vimrc_bak
+fi
+
+# copy new vimrc to ~/.vimrc
+cp vimrc $vimrc
+
+# delete old backup of ~/.vim if it exists
+if [[ -d $vim_bak ]] && [[ -w $vim_bak ]]
 then
   rm -rf $vim_bak
 fi
 
-if [ -d $vim_dir ] && [ -w $vim_dir ]
+# backup current .vim folder
+if [[ -d $vim_dir ]] && [[ -w $vim_dir ]]
 then
   mv $vim_dir $vim_bak
 fi
+
+# create new ~/.vim folder and copy contents
 mkdir $vim_dir
 cp -r after $vim_dir/after
 cp -r autoload $vim_dir/autoload
